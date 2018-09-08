69dc742e7c1c8cbc4528bc8917bcbca6ba3d77b0
unknown <laeirene59@gmail.com>
2018-09-08 09:05:52 -0400

    Backend

--- Project-SAME/models/muscle.js
+++ Project-SAME/models/muscle.js
@@ -1,29 +1,36 @@
-module.exports = function(sequelize, DataTypes) {
+module.exports = function (sequelize, DataTypes) {
   var Muscle = sequelize.define("Muscle", {
-  //   id: {
-  //     autoIncrement: true,
-  //     primaryKey: true,
-  //     type: DataTypes.INTEGER
-  //   },
-    Text: {
+    //   id: {
+    //     autoIncrement: true,
+    //     primaryKey: true,
+    //     type: DataTypes.INTEGER
+    //   },
+    value: {
       type: DataTypes.STRING,
       allowNull: false,
       primaryKey: true,
       len: [1]
-      
+
+    },
+    text: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      primaryKey: true,
+      len: [1]
+
     }
   }, {
       timestamps: false
-  });
+    });
 
-  Muscle.associate = function(models) {
+  Muscle.associate = function (models) {
     // We're saying that a Muscle should belong to an Author
     // A Muscle can't be created without an Author due to the foreign key constraint
-  //   Muscle.belongsTo(models.User, {
-  //     foreignKey: {
-  //       allowNull: false
-  //     }
-  //   });
+    //   Muscle.belongsTo(models.User, {
+    //     foreignKey: {
+    //       allowNull: false
+    //     }
+    //   });
     Muscle.hasMany(models.Exercise, {
       foreignKey: 'muscle',
       onDelete: "cascade"
