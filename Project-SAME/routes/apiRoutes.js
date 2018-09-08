37574df871b4e3f524d9fc911a2e168ffb1e8f4f
unknown <laeirene59@gmail.com>
2018-09-08 02:27:48 -0400

    So we can look together

--- Project-SAME/routes/apiRoutes.js
+++ Project-SAME/routes/apiRoutes.js
@@ -12,6 +12,15 @@ module.exports = function (app) {
     });
   });
 
+  app.get("/api/muscles", function(req, res) {
+    // Here we add an "include" property to our options in our findAll query
+    // We set the value to an array of the models we want to include in a left outer join
+    // In this case, just db.Post
+    db.Muscle.findAll().then(function(dbMuscle) {
+      res.json(dbMuscle);
+    });
+  });
+
   app.get("/api/plans", function(req, res) {
     // Here we add an "include" property to our options in our findAll query
     // We set the value to an array of the models we want to include in a left outer join
